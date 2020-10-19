e1e56efc4dfa63dcf3a638f1d82f05102f4cacad
iktumi <pgneto@gmail.com>
2020-10-19 10:51:00 +0100

    Code clean up

--- src/android/NotificationManager.java
+++ src/android/NotificationManager.java
@@ -1,15 +1,12 @@
 package com.g.cordova.plugin;
-// The native Toast API
+
 import android.content.Context;
-import android.widget.Toast;
-// Cordova-required packages
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import android.content.Context;
 
 public class NotificationManager extends CordovaPlugin {
     private static final String DURATION_LONG = "long";
@@ -18,10 +15,7 @@ public class NotificationManager extends CordovaPlugin {
                            final CallbackContext callbackContext) {
 		boolean result = true;
 
-        // Verify that the user sent a 'show' action
-        if (action.equals("show")) {
-            result =  showMessage(args, callbackContext);
-        } else if (action.equals("cancelAll")) {
+        if (action.equals("cancelAll")) {
             result = cancelAllNotifications(callbackContext);
         } else {
             callbackContext.error("\"" + action + "\" is not a recognized action.");
@@ -32,27 +26,6 @@ public class NotificationManager extends CordovaPlugin {
         return result;
     }
 
-    public boolean showMessage(JSONArray args, final CallbackContext callbackContext){
-        String message;
-        String duration;
-        try {
-            JSONObject options = args.getJSONObject(0);
-            message = options.getString("message");
-            duration = options.getString("duration");
-        } catch (JSONException e) {
-            callbackContext.error("Error encountered: " + e.getMessage());
-            return false;
-        }
-        // Create the toast
-        Toast toast = Toast.makeText(cordova.getActivity(), message,
-                DURATION_LONG.equals(duration) ? Toast.LENGTH_LONG : Toast.LENGTH_SHORT);
-        // Display toast
-        toast.show();
-        // Send a positive result to the callbackContext
-        PluginResult pluginResult = new PluginResult(PluginResult.Status.OK);
-        callbackContext.sendPluginResult(pluginResult);
-        return true;
-    }
 
     public boolean cancelAllNotifications(final CallbackContext callbackContext) {
         Context context = this.cordova.getActivity().getApplicationContext();
--- www/notificationManager.js
+++ www/notificationManager.js
@@ -1,15 +1,6 @@
 // Empty constructor
 function NotificationManager() {}
 
-// The function that passes work along to native shells
-// Message is a string, duration may be 'long' or 'short'
-NotificationManager.prototype.show = function(message, duration, successCallback, errorCallback) {
-  var options = {};
-  options.message = message;
-  options.duration = duration;
-  cordova.exec(successCallback, errorCallback, 'NotificationManager', 'show', [options]);
-}
-
 NotificationManager.prototype.cancelAll = function( successCallback, errorCallback) {
   var options = {}; 
   cordova.exec(successCallback, errorCallback, 'NotificationManager', 'cancelAll', [options]);
